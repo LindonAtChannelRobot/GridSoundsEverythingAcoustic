82d80f5ced215b9336af14e3619a03064341b85f
lindon <lindon@channelrobot.com>
2020-10-20 13:54:59 +0100

    mod selectors and voice params

---
+++ Scripts/ScriptProcessors/EVA02/ArpHolder.js
@@ -0,0 +1,21 @@
+ function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+    //Message.ignoreEvent(true);
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
